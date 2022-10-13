97e3429bace6e85183effbe7291d9738a60d1119
Sander Brijer <sanderbrijer@gmail.com>
2022-10-13 10:36:33 +0100

    Initial Commit

---
+++ Pokedex 646235/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pokedex 646235
+//
+//  Created by user225611 on 10/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pokedex 646235/Pokedex_646235App.swift
@@ -0,0 +1,17 @@
+//
+//  Pokedex_646235App.swift
+//  Pokedex 646235
+//
+//  Created by user225611 on 10/13/22.
+//
+
+import SwiftUI
+
+@main
+struct Pokedex_646235App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
